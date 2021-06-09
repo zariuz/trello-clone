5302c92727061a3a3d35c701bd160c1b30745a21
zariuz <nechetov@hotmail.com>
2021-06-09 23:42:53 +0300

    add Card NewItem and Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import {Column} from './Column';
+import {Card} from './Card';
+import {AddNewItem} from './AddNewItem';
 import {AppContainer} from './styles';
 
 export const App = () => {
-  return <AppContainer>Columns will go here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
+  );
 };
